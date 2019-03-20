3bc3eb52935a5a6c24197eac3c26b4af7c4d62b1
Juan Camilo Marin Navia <jcmarinn@eafit.edu.co>
2019-03-19 20:31:23 -0500

    JuanCamilo

---
+++ Juan Camilo.js
@@ -0,0 +1,14 @@
+class estudiante{
+    
+    constructor(nombre,email,id,nivel){
+        this.nombre=nombre;
+        this.email=email;
+        this.id=id;
+        this.nivel=nivel;
+    }
+
+    
+}
+
+var estudiante1=estudiante("juanca","jc@email.com",123456,"gennin");
+var estudiante2=estudiante("andres","ac@email.com",654321,"gennin2");
