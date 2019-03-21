ad5494f7d384f08c9a8af268041ad98dc3031754
jorge gaviria <jorge.gaviria@quipux.com>
2019-03-20 21:13:32 -0500

    Fix Office Lint error

--- models/Country.js
+++ models/Country.js
@@ -1,10 +1,10 @@
 class Country {
-    constructor(name, office, totalStudents, totalSenseis) {
-        this.Name = nombre;
-        this.Offices = sedes;
-        this.TotalStudents = totalEstudiantes;
-        this.TotalSenseis = totalSenseis;
-    }
+  constructor (name, office, totalStudents, totalSenseis) {
+    this.Name = nombre
+    this.Offices = sedes
+    this.TotalStudents = totalEstudiantes
+    this.TotalSenseis = totalSenseis
+  }
 }
 
-module.exports = Country;
+module.exports = Country
--- models/Office.js
+++ models/Office.js
@@ -1,10 +1,10 @@
 class Office {
-    constructor(nombre, ciudad, estudiantes, senseis) {
-        this.nombre;
-        this.ciudad;
-        this.estudiantes;
-        this.senseis;
-    }
+  constructor (nombre, ciudad, estudiantes, senseis) {
+    this.nombre = nombre
+    this.ciudad = ciudad
+    this.estudiantes = estudiantes
+    this.senseis = senseis
+  }
 }
 
-module.exports = Office;
+module.exports = Office
