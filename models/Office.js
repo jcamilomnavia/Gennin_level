90e7c3285d5a0c0e92f331d36ab8fcbef186e614
Juan Camilo Marin Navia <jcmarinn@eafit.edu.co>
2019-03-20 20:58:55 -0500

    Fixed parameters constructor

--- models/Office.js
+++ models/Office.js
@@ -1,10 +1,10 @@
 class Office {
-    constructor(nombre, ciudad, estudiantes, senseis) {
-        this.nombre;
-        this.ciudad;
-        this.estudiantes;
-        this.senseis;
-    }
+  constructor (nombre, ciudad, estudiantes, senseis) {
+    this.nombre
+    this.ciudad
+    this.estudiantes
+    this.senseis
+  }
 }
 
-module.exports = Office;
+module.exports = Office
