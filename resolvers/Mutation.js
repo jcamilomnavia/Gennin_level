1dba4970c1f37b352472e960a0bf1805680a13b3
jorge gaviria <jorge.gaviria@quipux.com>
2019-03-28 21:41:37 -0500

    Add featrue addToSet

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -6,9 +6,12 @@ const UpdateUser = (_, args) => userAction.updateUser(args.id, args.data)
 const CreatePost = (_, args) => postAction.createPost(args.data)
 const UpdatePost = (_, args) => postAction.updatePost(args.id, args.data)
 
+const AddPost = (_, args) => userAction.addPost(args.id, args.post)
+
 module.exports = {
   CreateUser,
   UpdateUser,
   CreatePost,
-  UpdatePost
+  UpdatePost,
+  AddPost
 }
